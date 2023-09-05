1300ba37f3b6e1dff1ce9dbbb318b5b68d733b41
Alberto <albertobarbosa0003@gmail.com>
2023-09-05 14:22:06 -0300

    Implementing stylizations with stitches, routes, next.js concepts, and resolving bugs.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-
+import { globalStyles } from '@componet/styles/global'
 import type { AppProps } from 'next/app'
+import logo from '../assets/logo.svg'
+import { Container, Header } from '@componet/styles/pages/app';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <Container>
+      <Header>
+        <img src={logo.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
