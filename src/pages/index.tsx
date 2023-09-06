4802ca694d3e57986f5c038f915ed8889d2a449a
Alberto <albertobarbosa0003@gmail.com>
2023-09-06 12:38:26 -0300

    Applying styling to the front end, carousel, installing packages for the carousel, implementing styling logic for image display on the front end.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,59 @@
+import { HomeContainer, Product } from "@componet/styles/pages/home";
+import Image from "next/image";
+
+import { useKeenSlider } from 'keen-slider/react'
+
+import shirt1 from '../assets/shirt1.png';
+import shirt2 from '../assets/shirt2.png';
+import shirt3 from '../assets/shirt3.png';
+
+import 'keen-slider/keen-slider.min.css';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  });
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={shirt1} width={520} height={480} alt="Camisa 1" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt2} width={520} height={480} alt="Camisa 2" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="Camisa 3" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} width={520} height={480} alt="Camisa 3" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+    </HomeContainer>
   )
 }
