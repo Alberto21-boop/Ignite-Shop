f5ddf13035a9eb948e149e8d179995cf8138c530
Alberto <albertobarbosa0003@gmail.com>
2023-09-12 15:04:08 -0300

    Updating requests and implementing packages to improve API consumption.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,57 +3,69 @@ import Image from "next/image";
 
 import { useKeenSlider } from 'keen-slider/react'
 
-import shirt1 from '../assets/shirt1.png';
-import shirt2 from '../assets/shirt2.png';
-import shirt3 from '../assets/shirt3.png';
-
 import 'keen-slider/keen-slider.min.css';
+import { stripe } from "@componet/lib/stripe";
+import { GetStaticProps } from "next";
+import { Stripe } from 'stripe';
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
       spacing: 48,
     }
   });
+
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={shirt1} width={520} height={480} alt="Camisa 1" />
+      {products.map(products => {
+        return (
+          <Product key={products.id} className="keen-slider__slide">
+            <Image src={products.imageUrl} width={520} height={480} alt="Camisa 1" />
 
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={shirt2} width={520} height={480} alt="Camisa 2" />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={shirt3} width={520} height={480} alt="Camisa 3" />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={shirt3} width={520} height={480} alt="Camisa 3" />
-
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
+            <footer>
+              <strong>{products.name}</strong>
+              <span>{products.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
     </HomeContainer>
   )
+}
+
+// é neste trecho de código que estamos fazendo a requisição da API
+// ou seja é aqui que bate na API lá na pasta lib
+
+export const getStaticProps: GetStaticProps = async () => {
+  // devemos trocar o getServerSideProps por getStaticProps
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(products => {
+    const price = products.default_price as Stripe.Price
+    return {
+      id: products.id,
+      name: products.name,
+      imageUrl: products.images[0],
+      price: price.unit_amount,
+    }
+  });
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2,
+  }
 }
